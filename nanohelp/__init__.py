ead320de5b97c3b96099514ee35539afb888c207
Nate Schultz <nate.schultz@outlook.com>
2023-07-27 22:51:32 -0400

    fix import

--- nanohelp/__init__.py
+++ nanohelp/__init__.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 # include these files in the build
-from nanohelp.transaction import Transaction
 from nanohelp.wallet import Wallet
 
 # imported into setup.py
